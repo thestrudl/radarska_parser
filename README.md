0d20dec48b3b819ef3561ff68578d1883a73340f
thestrudl <rok.vidmar1997@gmail.com>
2024-10-15 14:08:56 +0200

    Major refactor, Readme

---
+++ README.md
@@ -0,0 +1,39 @@
+# Slovenian Radar Data Storm Detector
+
+This Python script parses Slovenian radar data and detects storms based on specified locations. Users can input location names or addresses, and if the location is not already in the system, it can be added for future use.
+
+## Features
+
+- Download radar images for storm detection.
+- Check for storms based on pixel values in the radar image.
+- Store and manage locations in a CSV file.
+- Geocode new location addresses to get their coordinates.
+- Flexible radius setting for storm detection.
+
+## Requirements
+
+- Python 3.x
+- Required packages:
+  - `argparse`
+  - `requests`
+  - `Pillow`
+  - `pandas`
+  - `numpy`
+  - `geopy`
+
+You can install the required packages using pip:
+
+```bash
+pip install requests Pillow pandas numpy geopy
+
+## Usage
+```bash
+python radarska_parser.py -n <location_name> -l <location_address> -r <radius>
+
+```bash
+python script.py -n "Existing Location Name"
+
+
+```bash
+python script.py -n "New Location Name" -l "123 Main St, Ljubljana, Slovenia" -r 5
+
--- radarska_parser.py
+++ radarska_parser.py
@@ -8,95 +8,178 @@ import sys
 import pandas as pd
 import numpy as np
 import math
+import csv
+import os
+from os.path import exists
+from geopy.geocoders import Nominatim
 
-# If running from root be careful to use abosulte filepaths
-
-# Link to radarska
+# Link to radar image
 radarska_url = "https://meteo.arso.gov.si/uploads/probase/www/observ/radar/si0-rm.gif"
 
 
-def get_radarska():
-    r = requests.get(radarska_url, stream=True)  # Requests image in link
+def safe_create_dir(directory):
+    if not os.path.exists(directory):
+        os.makedirs(directory)
 
+
+def get_radarska():
+    r = requests.get(radarska_url, stream=True)
     date_time = datetime.now()
     dt_string = date_time.strftime("%Y%m%d_%H:%M:%S")
-    dir = "./radarske/"  # Set directory for saving radaska
-    # Adjust file name and path HERE, to run from root you have to use absolute filepath
-    filename = dir+dt_string+"_radarska"
-    # Exception for wrong file path
-    #################
-    # Checks if there is maybe something wrong with paths
-    folderchecker = subprocess.run(['bash', dir], stderr=subprocess.PIPE)
+    dir = "./radar_image/"
+    safe_create_dir(dir)
+    filename = dir + dt_string + "_radarska"
+
+    folderchecker = subprocess.run(["bash", dir], stderr=subprocess.PIPE)
     if folderchecker.returncode:
-        if b'No such file or directory' in folderchecker.stderr:
-            print('Directory is wrong or missing, check if filepath is correct, currently set directory is: '+dir)
+        if b"No such file or directory" in folderchecker.stderr:
+            print(
+                "Directory is wrong or missing, check if filepath is correct, currently set directory is: "
+                + dir
+            )
             sys.exit(1)
-    #################
-    if r.status_code == 200:  # If http transfer of request is succesful
+
+    if r.status_code == 200:
         r.raw.decode_content = True
-        with open(filename, 'wb') as f:
-            shutil.copyfileobj(r.raw, f)  # Save file
-            # Print("Imamo radarsko") #debug
-        return filename  # Returns saved file
+        with open(filename, "wb") as f:
+            shutil.copyfileobj(r.raw, f)
+        return filename
     else:
-        print("Something went wrong with downloading picture")
+        print("Something went wrong with downloading the picture")
 
 
 def read_pixel(image_to_analyze, x, y, radius):
     im = Image.open(image_to_analyze)
     pix = im.load()
-    # Colors from blue to violet
-    # 19, 18, 17, 16, 15, 14, 13, 12, 11, 10 yellow, 9, 8, 7 red, 6, 5 ,4
-    storm_flag = 0  # Storm no storm flag
-    # Array to analyze if there is any critical zones for storm
+    storm_flag = 0
     sweep_area_array = []
-
-    # Rounding to get numbers appropriate to select pixels
-
     x = round(x)
     y = round(y)
     radius = math.ceil(radius)
-    for x in range(x-radius, x+radius):
+    for x in range(x - radius, x + radius):
         for y in range(y - radius, y + radius):
-            sweep_area_array.append(pix[x, y])  # Appends values of area sweeped into array for if below
+            sweep_area_array.append(pix[x, y])
 
-    # print(sweep_area_array) #prints array for debug
-    if any(storm in range(4, 11) for storm in sweep_area_array):  #in this range set sensitivity of the storm alert
-        storm_flag = 1 
+    # This if defines the sensitivity of storm alert (color range)
+    if any(storm in range(4, 11) for storm in sweep_area_array):
+        storm_flag = 1
     else:
         storm_flag = 0
     return storm_flag
 
+
 def send_alert(storm_flag):
-    if storm_flag == 1:  # Do something if there is STORM
+    if storm_flag == 1:
         print(datetime.now())
         print("Storm ALERT !!!!!")
-    else:  # Do something if there is no storm
+    else:
         print("No storm")
 
-#put in while 1 with timer if you want script to run continously
 
-def get_data_from_csv(name):
+def get_location_coordinates(name):
     locations_df = pd.read_csv("./locations.csv")
-    index=np.where(locations_df["name"] == name)[0][0] #gets row index that corelates to entry in CSV in scalar/int form
-    x = locations_df.iloc[index,4]
-    y = locations_df.iloc[index,5]
-    radius = locations_df.iloc[index,7]
-    return x, y, radius
+    # Find the indices where the name matches
+    indices = np.where(locations_df["name"] == name)[0]
+
+    if len(indices) > 0:  # Check if there are any matching indices
+        index = indices[0]
+        x = locations_df.iloc[index, 4]
+        y = locations_df.iloc[index, 5]
+        radius = locations_df.iloc[index, 7]
+        return x, y, radius
+    else:
+        print("No location with that name found")
+        return None  # Return None or any other appropriate value
+
+
+# New location handling and CSV writing functions
+def get_locationdata(location):
+    geolocator = Nominatim(user_agent="MyApp")
+    location = geolocator.geocode(location)
+    lat = location.latitude
+    lon = location.longitude
+    return (lat, lon)
+
+
+def transform_lonlat_to_xy(lat, lon, radius):
+    x = lon * 152.302 - 1838.83
+    y = lat * (-224.91) + 10710.4
+    radiuspx = radius * 2.012
+    return (x, y, radiuspx)
+
+
+def raw_data_to_csv(name, location, lat, lon, x, y, radius, radiuspx):
+    filename = "locations.csv"
+    header = ["name", "location", "lat", "lon", "x", "y", "radius", "radiuspx"]
+    row_data = [name, location, lat, lon, x, y, radius, radiuspx]
+    names_list = []
+
+    file_locations_exists = exists("./locations.csv")
+    if not file_locations_exists:
+        with open(filename, "w", encoding="UTF-8", newline="") as write_obj:
+            csv_writer = csv.writer(write_obj)
+            csv_writer.writerow(header)
+            csv_writer.writerow(row_data)
+    else:
+        with open(filename, "a+", newline="") as readwrite_obj:
+            read_csv = csv.reader(readwrite_obj, delimiter=",")
+            for row in read_csv:
+                names_list.append(row[0])
+
+            if name in names_list:
+                print("That name is already used, name must be unique")
+            else:
+                csv_writer = csv.writer(readwrite_obj)
+                csv_writer.writerow(row_data)
+
 
 def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("-n","--name", type=str, help="Specify your location using name from list of locations in locations.csv", required=True)
+    parser = argparse.ArgumentParser(
+        description="Parse Slovenian radar data and detect storms, allowing input of location names or manual addresses. If you have not added location yet use -n -l and -r arguments to specify where we need to check. If you location is already in csv file just specify the name and we will check location for storms.",
+        epilog="Make sure locations.csv contains valid locations and radar data is accessible.",
+    )
+    parser.add_argument(
+        "-n",
+        "--name",
+        type=str,
+        help="Specify a location using a name from locations.csv or input a new one.",
+    )
+    parser.add_argument(
+        "-l",
+        "--location",
+        type=str,
+        help="Specify a new location address (optional, adds to CSV).",
+    )
+    parser.add_argument(
+        "-r",
+        "--radius",
+        type=float,
+        default=1.0,  # Set default value to 1 km
+        help="Set radius in km from center location (default=1km).",
+    )
+
     args = parser.parse_args()
     name = args.name
+    location = args.location
+    radius = args.radius
 
-    x,y,radius = get_data_from_csv(name)
+    if location and radius and name:
+        (lat, lon) = get_locationdata(location)
+        (x, y, radiuspx) = transform_lonlat_to_xy(lat, lon, radius)
+        raw_data_to_csv(name, location, lat, lon, x, y, radius, radiuspx)
+    elif name:
+        x_y_radius = get_location_coordinates(name)
+        if x_y_radius is None:
+            print("Exiting program due to missing location coordinates.")
+            sys.exit()  # Exit the program
+        file_to_analyze = get_radarska()
+        storm_flag = read_pixel(
+            file_to_analyze, x_y_radius[0], x_y_radius[1], x_y_radius[2]
+        )
+        send_alert(storm_flag)
+    else:
+        parser.print_help()
 
-    file_to_analyze = get_radarska() #fetches latest radarska for ARSO and saves file and path
 
-    storm_flag = read_pixel(file_to_analyze, x, y, radius) #insert image to analyze here, returns 0 (no storm), 1 (storm)
-
-    send_alert(storm_flag) #Does something dependent on the storm flag
-        
-
-main()
+if __name__ == "__main__":
+    main()
